8af624661df9bf79bdfde032ed42ec5c80554320
Julian Schelker <julian.schelker@adnovum.ch>
2018-09-06 00:15:12 +0200

    refactor mapgen

--- source/mapgen/split.lua
+++ source/mapgen/split.lua
@@ -7,57 +7,43 @@ require "libs.lua.functionBindings"
 
 local split={}
 
-local spawn_size = 64
-local ressSize = 8
-
-function generate_water_in_chunk(x,y,surface) -- coords are left_top of chunk
-	local tiles = square({{-2,-5},{2,5}},translate(x+16,y+16,generate_water_tile))
-	surface.set_tiles(tiles, true)
-end
+local spawn_size = 3 -- radius amount of chunks starting area per force (3 minimum for resources config below)
+local split_border_radius = 5 -- black border where forces can't cross
 
 local startingResource = {
-	["-2,-2"]={"iron-ore",8,200},
-	["-2,-1"]={"copper-ore",7,200},
-	["-1,-2"]={"stone",8,200},
-	["-1,-1"]={"coal",8,400},
-	["-1,1"]= generate_water_in_chunk,
-	
-	["1,-2"]={"iron-ore",8,200},
-	["1,-1"]={"copper-ore",7,200},
-	["0,-2"]={"stone",8,200},
-	["0,-1"]={"coal",8,400},
-	["0,1"]=generate_water_in_chunk
+	["3,3"]={"iron-ore",8,8,200},
+	["3,2"]={"copper-ore",7,7,200},
+	["2,2"]={"stone",8,8,200},
+	["2,3"]={"coal",8,8,400},
+	["1,3"]={"water",2,5}	
 }
 
+--[[ Text representation of the map:
+
+   |
+   |
+   |
+---X---
+   |  
+   | SC
+   |WcI
 
 
-function generate_border_tile(x,y,tiles)
-	if x>=-5 and x<= 5 then
-		table.insert(tiles, {name="out-of-map",position={x,y}})
-	end
+]]--
+
+function pos_to_chunkPos(coords)
+	local chunkX = math.floor((coords.x or coords[1]) / 32)
+	local chunkY = math.floor((coords.y or coords[2]) / 32)
+	return { x = chunkX, y = chunkY }
 end
 
-function generate_spawn_tile(x,y)
-	if x>= -spawn_size+5 and x<= spawn_size-5 and y>=-spawn_size+5 and y<=spawn_size-5 then
-		return {name="grass-1",position={x,y}}
-	elseif x>= -spawn_size and x<= spawn_size and y>=-spawn_size and y<=spawn_size then
-		return {name="dirt-7",position={x,y}}
-	end
-end
-
-function generate_resource(x,y,name,amount)
-	return {name=name,position={x,y},amount=amount}
-end
-
-function generate_water_tile(x,y)
-	return {name="water",position={x,y}}
-end
-	
 
 function is_spawn_chunk(left_top)
-	return left_top.x>= -spawn_size and left_top.x < spawn_size
-		and left_top.y>= -spawn_size and left_top.y < spawn_size
+	local chunkPos = pos_to_chunkPos(left_top)
+	return chunkPos.x >= -spawn_size and chunkPos.x <= spawn_size and
+		chunkPos.y >= -spawn_size and chunkPos.y <= spawn_size
 end
+
 
 function square(area, lambda)
 	local result = {}
@@ -74,11 +60,13 @@ function square(area, lambda)
 	return result
 end
 
+
 function translate(tx,ty,lambda)
 	return function(x,y)
 		return lambda(x+tx, y+ty)
 	end
 end
+
 
 function foreach(tabl, lambda)
 	for _,x in pairs(tabl) do
@@ -86,50 +74,114 @@ function foreach(tabl, lambda)
 	end
 end
 
-function split.on_init()
-	game.create_force("player2")
-	game.forces['player'].set_spawn_position({-15,0},game.surfaces[1])
-	game.forces['player2'].set_spawn_position({15,0},game.surfaces[1])	
+
+-- Tile creators
+
+function generate_spawn_tile(x,y)
+	local b = 5 -- border of dirt on the outside of spawn area
+	local outerAreaLT = -spawn_size * 32
+	local outerAreaRB = spawn_size * 32 + 32 -- chunk 0,0 is used for water
+	if x < outerAreaLT + b or x > outerAreaRB - b or
+		y < outerAreaLT + b or y > outerAreaRB - b then
+		return {name="dirt-7",position={x,y}}
+	else
+		return {name="grass-1",position={x,y}}
+	end
 end
 
 
-function split.on_chunk_generated(event)
+function generate_resource_tile(x,y,name,amount)
+	return {name = name, position = {x,y}, amount = amount}
+end
 
+
+function generate_water_tile(x,y)
+	return {name = "water", position = {x,y}}
+end
+
+function generate_border_tile(x,y)
+	return {name = "out-of-map", position = {x,y}}
+end
+
+
+-- Chunk modifiers / generators
+
+function chunk_generator_spawn_tiles(event)
+	-- place floor tiles (grass + dirt), remove decoratives
+	if not is_spawn_chunk(event.area.left_top) then return end
+	local tiles = square(event.area, generate_spawn_tile)
+	event.surface.set_tiles(tiles, true)
+	event.surface.destroy_decoratives(event.area)
+end
+
+
+function chunk_generator_spawn_clean_area(event)
+	if not is_spawn_chunk(event.area.left_top) then return end
+	local entities = event.surface.find_entities_filtered{area=event.area, type= {"resource","tree"}}
+	for _,entity in pairs(entities) do
+		entity.destroy()
+	end
+end
+
+
+function chunk_generator_spawn_starting_resource(event)
+	if not is_spawn_chunk(event.area.left_top) then return end
+	local chunkPos = pos_to_chunkPos(event.area.left_top)
+	local chunkStr = math.abs(chunkPos.x) .. "," .. math.abs(chunkPos.y)
+	local resGen = startingResource[chunkStr]
+	if resGen == nil then return end
+	local entities = 
+		square({{-resGen[2],-resGen[3]},{resGen[2],resGen[3]}},
+		translate(event.area.left_top.x+16,event.area.left_top.y+16, function(x,y)
+			if resGen[1] == "water" then
+				return generate_water_tile(x,y)
+			else
+				return generate_resource_tile(x,y,resGen[1],resGen[4])
+			end
+		end))
+	if resGen[1] == "water" then
+		event.surface.set_tiles(entities, true)
+	else
+		foreach(entities,event.surface.create_entity)
+	end
+end
+
+
+function chunk_generator_split_base_borders(event)
+	local chunkPos = pos_to_chunkPos(event.area.left_top)
+	if chunkPos.x ~= 0 and chunkPos.y ~= 0 then return end
+	if chunkPos.x == 0 then
+		local r = split_border_radius
+		local tiles = square({{-r, -16}, {r, 15}},
+			translate(event.area.left_top.x + 16, event.area.left_top.y + 16, 
+			generate_border_tile))
+		event.surface.set_tiles(entities, true)
+	end
+	if chunksPos.y == 0 then
+		local r = split_border_radius
+		local tiles = square({{-16, -r}, {15, r}},
+			translate(event.area.left_top.x + 16, event.area.left_top.y + 16, 
+			generate_border_tile))
+		event.surface.set_tiles(entities, true)
+	end
+end
+
+
+-- all the different modifications on top of the vanilla map generation in order:
+local chunk_generators = {
+	chunk_generator_spawn_tiles,
+	chunk_generator_spawn_clean_area,
+	chunk_generator_spawn_starting_resource,
+	chunk_generator_split_base_borders
+}
+
+
+
+function split.on_chunk_generated(event)
   -- only check nauvis 
 	if event.surface ~= game.surfaces[1] then return end
+
 	
-	if is_spawn_chunk(event.area.left_top) then
-		local tiles = square(event.area, generate_spawn_tile)
-		event.surface.set_tiles(tiles, true)
-		event.surface.destroy_decoratives(event.area)
-		local entities = event.surface.find_entities_filtered{area=event.area, type= {"resource","tree"}}
-		for _,entity in pairs(entities) do
-			entity.destroy()
-		end
-		
-		local chunkCoords = {math.floor(event.area.left_top.x/32),
-			math.floor(event.area.left_top.y/32)}
-		local chunkStr = chunkCoords[1]..","..chunkCoords[2]
-		local resGen = startingResource[chunkStr]
-		if resGen ~= nil and type(resGen)=="function" then
-			resGen(event.area.left_top.x, event.area.left_top.y,game.surfaces[1])
-		elseif resGen ~= nil then
-			local sur = game.surfaces[1]
-			local fs = resGen[2] -- field Size
-			local entities = square({{-fs,-fs},{fs,fs}},
-				translate(event.area.left_top.x+16,event.area.left_top.y+16,
-				bind34(generate_resource,resGen[1],resGen[3])))
-			foreach(entities,sur.create_entity)
-		end
-		
-	end
-	
-	if event.area.right_bottom.x < -32 or
-		event.area.left_top.x > 32 then
-		return
-	end
-	
-	-- xx(event.area)
 	
 	local tiles = {}
 	for x = event.area.left_top.x, event.area.right_bottom.x do
@@ -142,6 +194,12 @@ function split.on_chunk_generated(event)
 end
 
 
+function split.on_init()
+	game.create_force("player2")
+	game.forces['player'].set_spawn_position({-15,0},game.surfaces[1])
+	game.forces['player2'].set_spawn_position({15,0},game.surfaces[1])	
+end
+
 function split.on_player_created(event)
 	local nr = event.player_index
 	local force = nr % 2
@@ -150,7 +208,6 @@ function split.on_player_created(event)
 		game.players[nr].teleport( game.players[nr].force.get_spawn_position(game.surfaces[1]))
 	end
 end
-
 
 
 
